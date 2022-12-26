f75ae3d54dd9fff3126ae13bc8eff45dcba8b844
hg2072 <hg2072@126.com>
2022-12-26 11:10:58 +0800

    first commit

--- src/main/java/com/huangming/controller/HelloController.java
+++ src/main/java/com/huangming/controller/HelloController.java
@@ -1,2 +1,18 @@
-package com.huangming.controller;public class HelloController {
+package com.huangming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/HelloController")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(Model model){
+        //向模型中添加属性msg与值，可以在JSP页面中取出并渲染
+        model.addAttribute("msg","hello,SpringMVC");
+        //web-inf/jsp/hello.jsp
+        return "hello";
+    }
 }
